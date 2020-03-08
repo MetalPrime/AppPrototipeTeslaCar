ea7e3e45af233d29be9bb59541bdcbdb6726fbaa
Jhon Garcia <metalprime57@gmail.com>
2020-03-07 21:31:56 -0500

    update input register

--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -1,5 +1,15 @@
 package controller;
 
-public class RegisterController {
+import model.Logic;
 
+public class RegisterController {
+	private Logic logic;
+	
+	public RegisterController() {
+		logic = new Logic();
+	}
+	
+	public void getInfoPage(String username, String email, String password, String confirmedPassword) {
+		logic.registerUser(username, password, email);
+	}
 }
--- src/view/RegisterScreen.java
+++ src/view/RegisterScreen.java
@@ -1,13 +1,27 @@
 package view;
 
+import controlP5.ControlP5;
+import controller.RegisterController;
 import processing.core.PApplet;
 
 public class RegisterScreen {
 	
 	private PApplet app;
+	private ControlP5 cp5;
+	private RegisterController registerC;
+	private String[] inputsRegister = {"username","password","confirmedPassword","email"};
+	private String username,password,confirmedPassword,email;
+	
 	
 	public RegisterScreen(PApplet app) {
 		this.app = app;
+		cp5 = new ControlP5(app);
+		
+		registerC = new RegisterController();
+		
+		for(int i=0; i<inputsRegister.length; i++) {
+			cp5.addTextfield(inputsRegister[i]).setPosition((app.width/2)-100,30+(i*70)).setSize(200,40).setAutoClear(true);
+		}
 	}
 
 	public void paint() {
