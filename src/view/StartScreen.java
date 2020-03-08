9bd6f3b743c08915e23eb34ba3f17c36b3afcc13
Jhon Garcia <metalprime57@gmail.com>
2020-03-08 11:45:07 -0500

    update startScreen

--- src/view/StartScreen.java
+++ src/view/StartScreen.java
@@ -2,12 +2,14 @@ package view;
 
 import controlP5.ControlP5;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class StartScreen {
 	
 	private ControlP5 cp5;
 	private PApplet app;
 	private String[] bottons = {"Get Started","Sign In"};
+	private PImage back,logo;
 
 	public StartScreen(PApplet app) {
 		// TODO Auto-generated constructor stub
@@ -17,11 +19,18 @@ public class StartScreen {
 			System.out.println(bottons.length);
 			cp5.addBang(bottons[i]).setPosition(app.width/2-75,(i*70)+app.height/2).setSize(150,50);
 		}
+		
+		back = app.loadImage("./../Taller1Individual/data/Register/background.png");
+		logo = app.loadImage("./../Taller1Individual/data/Register/logo.png");
 	}
+	
+	
 
 	public void paint() {
 		// TODO Auto-generated method stub
 		app.background(180);
+		app.image (back,0,0);
+		app.image (logo,app.width/5,app.height/10);
 		
 	}
 
