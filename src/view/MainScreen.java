fce37aa601038a00ac599f909bf5d041e739e6ca
Jhon Garcia <metalprime57@gmail.com>
2020-03-07 19:21:34 -0500

    update Users

--- src/view/MainScreen.java
+++ src/view/MainScreen.java
@@ -1,10 +1,35 @@
 package view;
 
-public class MainScreen {
+import processing.core.PApplet;
+
+public class MainScreen extends PApplet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		PApplet.main(MainScreen.class.getName());
+	}
 
+	RegisterScreen registerS;
+
+	public void settings() {
+		size(360, 640);
+	}
+
+	public void setup() {
+		registerS = new RegisterScreen(this);
+				
+	}
+
+	public void draw() {
+		registerS.paint();
+	}
+
+	public void mouseClicked() {
+		registerS.clicked();
+	}
+
+	public void keyPressed() {
+		registerS.pressed();
 	}
 
 }
--- src/view/RegisterScreen.java
+++ src/view/RegisterScreen.java
@@ -1,5 +1,29 @@
 package view;
 
-public class RegisterScreen {
+import processing.core.PApplet;
 
+public class RegisterScreen {
+	
+	private PApplet app;
+	
+	public RegisterScreen(PApplet app) {
+		this.app = app;
+	}
+
+	public void paint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void clicked() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void pressed() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
