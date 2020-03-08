f66c00e89e5f4d3e2d716e807afb2ed64a51c0e5
Jhon Garcia <metalprime57@gmail.com>
2020-03-07 19:44:07 -0500

    add users

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,5 +1,21 @@
 package model;
 
-public class Logic {
+import java.util.ArrayList;
 
+public class Logic {
+	
+	private ArrayList<User> listUsers;
+	
+	public Logic() {
+		listUsers = new ArrayList<User>();
+	}
+	
+	public void registerUser(String username,String password,String email) {
+		for (User user : listUsers) {
+			System.out.println(user.getUsername());
+			System.out.println(user.getEmail());
+			System.out.println(user.getPassword());
+			System.out.println("===================");
+		}
+	}
 }
